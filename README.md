f4b0b792d0e3f30872f2cb6b874ef606bd1cb62a
SYAMRAJ <syamrajradhakrishnan@gmail.com>
2023-12-10 12:02:33 +0530

    completed work first upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # portfolio
 My first project
+<br>
+SYAMRAJ
---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ["Hardware and Network Engineer", "Fullstack Developer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
